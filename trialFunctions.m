2facd59ebc818d0d28a3cba4a9c18a942f85d442
maliksagar96 <malik.sagar97@gmail.com>
2020-07-12 22:18:12 +0530

    Added an interesting funtion

--- trialFunctions.m
+++ trialFunctions.m
@@ -1,13 +1,13 @@
-interval = .01;
+interval = 0.001;
 initialValue = 0;
-finalValue = pi;
+finalValue = 1;
 
 
 x = initialValue:interval:finalValue;
 
-
 y = sin(x);
 y1 = cos(x);        %Take final value pi/2 area should be 1 then
 y2 = exp(x);        %Take final value 1, area should be e-1 = 1.7183  
+y3 = log(x.*x + 1 )./(x+1);
 
-simpsonIntegral(interval,y)
+simpsonIntegral(interval,y3)
